b1e829af1be01b5f271a5b1658608a52e8824e13
SaraEOlivera <saraeolivera@gmail.com>
2025-01-10 10:31:41 -0300

    Se crea formulario de vista detallada

---
+++ Articulo/FrmDetalle.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Articulo
+{
+    partial class FrmDetalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigoDetalle = new System.Windows.Forms.Label();
+            this.txtCodigoDetalle = new System.Windows.Forms.TextBox();
+            this.txtPrecioDetalle = new System.Windows.Forms.TextBox();
+            this.lblPrecioDetalle = new System.Windows.Forms.Label();
+            this.txtMarcaDetalle = new System.Windows.Forms.TextBox();
+            this.lblCategoriaDetalle = new System.Windows.Forms.Label();
+            this.txtCategoriaDetalle = new System.Windows.Forms.TextBox();
+            this.lblMarcaDetalle = new System.Windows.Forms.Label();
+            this.txtDescripcionDetalle = new System.Windows.Forms.TextBox();
+            this.lblDescripcionDetalle = new System.Windows.Forms.Label();
+            this.txtNombreDetalle = new System.Windows.Forms.TextBox();
+            this.lblNombreDetalle = new System.Windows.Forms.Label();
+            this.btnVolverDetalle = new System.Windows.Forms.Button();
+            this.pboImagenUrlDetalle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboImagenUrlDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCodigoDetalle
+            // 
+            this.lblCodigoDetalle.AutoSize = true;
+            this.lblCodigoDetalle.Location = new System.Drawing.Point(64, 17);
+            this.lblCodigoDetalle.Name = "lblCodigoDetalle";
+            this.lblCodigoDetalle.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigoDetalle.TabIndex = 0;
+            this.lblCodigoDetalle.Text = "Código";
+            // 
+            // txtCodigoDetalle
+            // 
+            this.txtCodigoDetalle.Location = new System.Drawing.Point(122, 13);
+            this.txtCodigoDetalle.Name = "txtCodigoDetalle";
+            this.txtCodigoDetalle.Size = new System.Drawing.Size(295, 20);
+            this.txtCodigoDetalle.TabIndex = 1;
+            // 
+            // txtPrecioDetalle
+            // 
+            this.txtPrecioDetalle.Location = new System.Drawing.Point(122, 205);
+            this.txtPrecioDetalle.Name = "txtPrecioDetalle";
+            this.txtPrecioDetalle.Size = new System.Drawing.Size(295, 20);
+            this.txtPrecioDetalle.TabIndex = 3;
+            // 
+            // lblPrecioDetalle
+            // 
+            this.lblPrecioDetalle.AutoSize = true;
+            this.lblPrecioDetalle.Location = new System.Drawing.Point(66, 208);
+            this.lblPrecioDetalle.Name = "lblPrecioDetalle";
+            this.lblPrecioDetalle.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecioDetalle.TabIndex = 4;
+            this.lblPrecioDetalle.Text = "Precio";
+            // 
+            // txtMarcaDetalle
+            // 
+            this.txtMarcaDetalle.Location = new System.Drawing.Point(122, 130);
+            this.txtMarcaDetalle.Name = "txtMarcaDetalle";
+            this.txtMarcaDetalle.Size = new System.Drawing.Size(295, 20);
+            this.txtMarcaDetalle.TabIndex = 7;
+            // 
+            // lblCategoriaDetalle
+            // 
+            this.lblCategoriaDetalle.AutoSize = true;
+            this.lblCategoriaDetalle.Location = new System.Drawing.Point(47, 174);
+            this.lblCategoriaDetalle.Name = "lblCategoriaDetalle";
+            this.lblCategoriaDetalle.Size = new System.Drawing.Size(54, 13);
+            this.lblCategoriaDetalle.TabIndex = 6;
+            this.lblCategoriaDetalle.Text = "Categoría";
+            // 
+            // txtCategoriaDetalle
+            // 
+            this.txtCategoriaDetalle.Location = new System.Drawing.Point(122, 171);
+            this.txtCategoriaDetalle.Name = "txtCategoriaDetalle";
+            this.txtCategoriaDetalle.Size = new System.Drawing.Size(295, 20);
+            this.txtCategoriaDetalle.TabIndex = 9;
+            // 
+            // lblMarcaDetalle
+            // 
+            this.lblMarcaDetalle.AutoSize = true;
+            this.lblMarcaDetalle.Location = new System.Drawing.Point(64, 137);
+            this.lblMarcaDetalle.Name = "lblMarcaDetalle";
+            this.lblMarcaDetalle.Size = new System.Drawing.Size(37, 13);
+            this.lblMarcaDetalle.TabIndex = 8;
+            this.lblMarcaDetalle.Text = "Marca";
+            // 
+            // txtDescripcionDetalle
+            // 
+            this.txtDescripcionDetalle.Location = new System.Drawing.Point(122, 91);
+            this.txtDescripcionDetalle.Name = "txtDescripcionDetalle";
+            this.txtDescripcionDetalle.Size = new System.Drawing.Size(295, 20);
+            this.txtDescripcionDetalle.TabIndex = 11;
+            // 
+            // lblDescripcionDetalle
+            // 
+            this.lblDescripcionDetalle.AutoSize = true;
+            this.lblDescripcionDetalle.Location = new System.Drawing.Point(41, 98);
+            this.lblDescripcionDetalle.Name = "lblDescripcionDetalle";
+            this.lblDescripcionDetalle.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcionDetalle.TabIndex = 10;
+            this.lblDescripcionDetalle.Text = "Descripción";
+            // 
+            // txtNombreDetalle
+            // 
+            this.txtNombreDetalle.Location = new System.Drawing.Point(122, 53);
+            this.txtNombreDetalle.Name = "txtNombreDetalle";
+            this.txtNombreDetalle.Size = new System.Drawing.Size(295, 20);
+            this.txtNombreDetalle.TabIndex = 13;
+            // 
+            // lblNombreDetalle
+            // 
+            this.lblNombreDetalle.AutoSize = true;
+            this.lblNombreDetalle.Location = new System.Drawing.Point(64, 57);
+            this.lblNombreDetalle.Name = "lblNombreDetalle";
+            this.lblNombreDetalle.Size = new System.Drawing.Size(40, 13);
+            this.lblNombreDetalle.TabIndex = 12;
+            this.lblNombreDetalle.Text = "Detalle";
+            // 
+            // btnVolverDetalle
+            // 
+            this.btnVolverDetalle.Location = new System.Drawing.Point(436, 201);
+            this.btnVolverDetalle.Name = "btnVolverDetalle";
+            this.btnVolverDetalle.Size = new System.Drawing.Size(212, 26);
+            this.btnVolverDetalle.TabIndex = 14;
+            this.btnVolverDetalle.Text = "Volver";
+            this.btnVolverDetalle.UseVisualStyleBackColor = true;
+            this.btnVolverDetalle.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // pboImagenUrlDetalle
+            // 
+            this.pboImagenUrlDetalle.Location = new System.Drawing.Point(445, 17);
+            this.pboImagenUrlDetalle.Name = "pboImagenUrlDetalle";
+            this.pboImagenUrlDetalle.Size = new System.Drawing.Size(203, 174);
+            this.pboImagenUrlDetalle.TabIndex = 15;
+            this.pboImagenUrlDetalle.TabStop = false;
+            // 
+            // FrmDetalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 251);
+            this.Controls.Add(this.pboImagenUrlDetalle);
+            this.Controls.Add(this.btnVolverDetalle);
+            this.Controls.Add(this.txtNombreDetalle);
+            this.Controls.Add(this.lblNombreDetalle);
+            this.Controls.Add(this.txtDescripcionDetalle);
+            this.Controls.Add(this.lblDescripcionDetalle);
+            this.Controls.Add(this.txtCategoriaDetalle);
+            this.Controls.Add(this.lblMarcaDetalle);
+            this.Controls.Add(this.txtMarcaDetalle);
+            this.Controls.Add(this.lblCategoriaDetalle);
+            this.Controls.Add(this.lblPrecioDetalle);
+            this.Controls.Add(this.txtPrecioDetalle);
+            this.Controls.Add(this.txtCodigoDetalle);
+            this.Controls.Add(this.lblCodigoDetalle);
+            this.Name = "FrmDetalle";
+            this.Text = "FrmDetalle";
+            ((System.ComponentModel.ISupportInitialize)(this.pboImagenUrlDetalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigoDetalle;
+        private System.Windows.Forms.TextBox txtCodigoDetalle;
+        private System.Windows.Forms.TextBox txtPrecioDetalle;
+        private System.Windows.Forms.Label lblPrecioDetalle;
+        private System.Windows.Forms.TextBox txtMarcaDetalle;
+        private System.Windows.Forms.Label lblCategoriaDetalle;
+        private System.Windows.Forms.TextBox txtCategoriaDetalle;
+        private System.Windows.Forms.Label lblMarcaDetalle;
+        private System.Windows.Forms.TextBox txtDescripcionDetalle;
+        private System.Windows.Forms.Label lblDescripcionDetalle;
+        private System.Windows.Forms.TextBox txtNombreDetalle;
+        private System.Windows.Forms.Label lblNombreDetalle;
+        private System.Windows.Forms.Button btnVolverDetalle;
+        private System.Windows.Forms.PictureBox pboImagenUrlDetalle;
+    }
+}
